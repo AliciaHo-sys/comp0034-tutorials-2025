04bc8c917431b8c8a0083d7f1078b5f5e57a2568
nicholsons <sarah.sanders@ucl.ac.uk>
2026-01-19 19:53:13 +0000

    Added helpers for week 3 activities

--- src/data/mock_api.py
+++ src/data/mock_api.py
@@ -1,8 +1,8 @@
 """ Minimal code to return JSON data
 
  NB: This is NOT how to create a REST API. It is the simplest code possible to return the data
- in JSON format and lacks any of the expected validation and structure. Do not use this as an
- example for coursework 2!
+ in JSON format and lacks any of the expected validation and structure. This is fine for cw1 but
+ do not use this as an example for coursework 2!
 
  """
 import json
@@ -13,6 +13,7 @@ from typing import Callable
 import pandas as pd
 import uvicorn
 from fastapi import FastAPI, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import RedirectResponse
 
 
@@ -58,7 +59,8 @@ class ParalympicsData:
         tables: list of table names from the database
 
     Methods:
-        get_table(self, table_name): Gets the data from the specified table and returns it as JSON
+        get_table_as_json(self, table_name): Gets the data from the specified table and returns it as JSON
+        get_all_data(self): Gets data from joined tables and returns it as JSON
 
     """
 
@@ -78,14 +80,16 @@ class ParalympicsData:
         except Exception as e:
             raise RuntimeError(f"Error querying database tables: {e}") from e
 
-    def get_json(self, table_name):
+    def get_table_as_json(self, table_name):
         """ Method to return the specified table data from the paralympics .db file.
 
         Uses sqlite3 to access and query the database
         Only accepts a table name if it exists in the database
 
-        Returns:
+        Args:
+            table_name: name of the database table
 
+        Returns:
             json_data: json format data
         """
         try:
@@ -106,9 +110,63 @@ class ParalympicsData:
             if conn:
                 conn.close()
 
+    def get_all_data(self):
+        """ Method to return all data from the paralympics .db file.
+
+        Doesn't currently include games.url, games.highlights, or disabilities
+
+        Returns:
+            data: json format data
+
+        Raises:
+            e: Exception
+        """
+        sql = (
+            "SELECT country.country_name, games.event_type, games.year, games.start_date, "
+            "games.end_date, host.place_name, games.events, games.sports, games.countries, "
+            "games.participants_m, games.participants_f, games.participants, host.latitude, "
+            "host.longitude "
+            "FROM games "
+            "JOIN games_host ON games.id = games_host.games_id "
+            "JOIN host ON games_host.host_id = host.id "
+            "JOIN country ON host.country_id = country.id"
+        )
+        try:
+            conn = sqlite3.connect(self.database_file)
+            with conn:
+                conn.row_factory = sqlite3.Row  # Returns columns by names instead of tuples
+                cur = conn.cursor()
+                cur.execute(sql)
+                rows = cur.fetchall()
+                if not rows:
+                    return []
+                data = [dict(row) for row in rows]
+                return data
+        except Exception as e:
+            raise RuntimeError(f"Error querying tables: {e}") from e
+        finally:
+            if conn:
+                conn.close()
+
 
 # Minimal code to generate a REST API app with JSON format data
 app = FastAPI(title="Mock Paralympics API")
+
+origins = [
+    "http://localhost",
+    "http://127.0.0.1",
+    "http://localhost:8050",  # dash default
+    "http://localhost:5000",  # flask default
+    "http://localhost:8501",  # streamlit default
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 data = ParalympicsData()
 _tables = data.tables
@@ -132,7 +190,7 @@ def _make_route(table_name: str) -> Callable:
 
     async def _route():
         try:
-            return data.get_json(table_name)
+            return data.get_table_as_json(table_name)
         except AttributeError:
             raise HTTPException(status_code=500, detail="ParalympicsData.get_json not implemented")
         except Exception as exc:
@@ -144,6 +202,18 @@ def _make_route(table_name: str) -> Callable:
 # create a GET route for each table at /{table}
 for _t in _tables:
     app.get(f"/{_t}", name=_t)(_make_route(_t))
+
+
+# Create a root to data for the charts
+@app.get("/all")
+async def get_all():
+    try:
+        return data.get_all_data()
+    except AttributeError:
+        raise HTTPException(status_code=500, detail="ParalympicsData.get_json not implemented")
+    except Exception as exc:
+        raise HTTPException(status_code=500, detail=str(exc))
+
 
 if __name__ == "__main__":
     uvicorn.run("src.data.mock_api:app", host="127.0.0.1", port=8000, reload=True)
